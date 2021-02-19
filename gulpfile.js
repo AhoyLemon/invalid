d4d6b34918047355a3d78d68473a17a560c8bfae
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2021-02-19 12:57:45 -0600

    Ugh. This sucks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 // Load plugins
 var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
     minifycss = require('gulp-minify-css'),
     notify = require('gulp-notify'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass');
 
 const notifier = require('node-notifier');
-
-
 
 /*
 
@@ -22,34 +20,22 @@ notifier.notify({
 });
 */
 
-
-/*
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
-*/
-
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('scss/cameo.scss')
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({ style: 'expanded', }))
+    .pipe(sourcemaps.write('.'))
     //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('public/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
     //.pipe(livereload(server))
     .pipe(gulp.dest('public/css'))
-    .pipe(notify({message:"Hello"}))
+    .pipe(notify({message:"Hello"}));
 });
+
+
+gulp.task('watch', function(){
+  gulp.watch('scss/*.scss', gulp.series(['styles'])); 
+})
